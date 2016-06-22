ca8edf5d52152dcc299a6889606e37e5f0f50dd9
Taylor Kennedy <taykenned@gmail.com>
2016-06-21 23:25:40 -1000

    adds a static directory

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, _ => {
   console.log(`listening on port ${ PORT }`);
